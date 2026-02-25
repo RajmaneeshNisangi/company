672d1b5cb3feed9b92beea4733e85c797e32b9f6
RajmaneeshNisangi <rajmaneesh.nisangi@gmail.com>
2026-02-25 15:53:55 +0530

    Built web
    Branch: main
    Commit: d97ddb89f5e36b152771570129753f09a471bb0f
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "588746059"
+    serviceWorkerVersion: "2684084887"
   }
 });
